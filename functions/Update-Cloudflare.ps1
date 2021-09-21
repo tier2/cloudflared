35f672b5d4d5cead6813d8405facef60d6d36cc1
tier2 <88996765+tier2@users.noreply.github.com>
2021-09-22 10:51:05 +1200

    Adjust update script

--- functions/Update-Cloudflare.ps1
+++ functions/Update-Cloudflare.ps1
@@ -1,9 +1,8 @@
 #requires -version 4.0
 #requires -RunAsAdministrator
 
-taskkill -f -im cloudflared.exe
 C:\Cloudflared\cloudflared\bin\cloudflared.exe service uninstall
-.\cloudflared.exe update
+C:\Cloudflared\cloudflared\bin\cloudflared.exe update
 C:\Cloudflared\cloudflared\bin\cloudflared.exe service install
 Write-Host "Adding custom ImagePath to for Cloudflared to point to C:\Cloudflared\config.yml"
 C:\Cloudflared\cloudflared\Resources\ImagePath.reg
