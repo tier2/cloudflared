119d599d86ca330b4a2ba17f9d5a51dfee1ffca9
tier2 <88996765+tier2@users.noreply.github.com>
2021-09-22 11:03:15 +1200

    Update Script

--- functions/Update-Cloudflare.ps1
+++ functions/Update-Cloudflare.ps1
@@ -1,10 +1,20 @@
 #requires -version 4.0
 #requires -RunAsAdministrator
-
-C:\Cloudflared\cloudflared\bin\cloudflared.exe service uninstall
+$Service = Get-Service cloudflared
+if($service){
+    C:\Cloudflared\cloudflared\bin\cloudflared.exe service uninstall
+}
+$Service = Get-Service cloudflared -erroraction "silentlycontinue"
+do{
+    Write-host "Waiting for service to finish uninstalling..." -ForegroundColor Yellow
+}until(!$service)
 C:\Cloudflared\cloudflared\bin\cloudflared.exe update
+Start-Sleep -Seconds 4
 C:\Cloudflared\cloudflared\bin\cloudflared.exe service install
-Write-Host "Adding custom ImagePath to for Cloudflared to point to C:\Cloudflared\config.yml"
+$Service = Get-Service cloudflared -erroraction "silentlycontinue"
+do{
+    Write-host "Waiting for service to finish installing..." -ForegroundColor Yellow
+}until($service)
 Write-Host "Adding custom ImagePath to for Cloudflared to point to C:\Cloudflared\config.yml"
 Invoke-Command {reg import C:\Cloudflared\cloudflared\Resources\ImagePath.reg *>&1 | Out-Null}
 $Service = Get-Service cloudflared
