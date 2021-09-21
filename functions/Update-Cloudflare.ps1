bd00507c94e423666121572723f4382dd5435e2d
tier2 <88996765+tier2@users.noreply.github.com>
2021-09-22 10:56:26 +1200

    Added better registry and service handling

--- functions/Update-Cloudflare.ps1
+++ functions/Update-Cloudflare.ps1
@@ -5,5 +5,13 @@ C:\Cloudflared\cloudflared\bin\cloudflared.exe service uninstall
 C:\Cloudflared\cloudflared\bin\cloudflared.exe update
 C:\Cloudflared\cloudflared\bin\cloudflared.exe service install
 Write-Host "Adding custom ImagePath to for Cloudflared to point to C:\Cloudflared\config.yml"
-C:\Cloudflared\cloudflared\Resources\ImagePath.reg
-Start-Service cloudflared
+Write-Host "Adding custom ImagePath to for Cloudflared to point to C:\Cloudflared\config.yml"
+Invoke-Command {reg import C:\Cloudflared\cloudflared\Resources\ImagePath.reg *>&1 | Out-Null}
+$Service = Get-Service cloudflared
+if($service.status -eq "Running"){
+    Stop-Service cloudflared
+    Start-Service cloudflared
+
+} else {
+    Start-Service cloudflared
+}
