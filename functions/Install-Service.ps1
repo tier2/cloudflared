66e165a60704308057cffa42b907143447fcc11b
tier2 <88996765+tier2@users.noreply.github.com>
2021-09-22 13:36:08 +1200

    Cleanup

--- functions/Install-Service.ps1
+++ functions/Install-Service.ps1
@@ -6,7 +6,7 @@ Write-Host "Install Cloudflared Service."
 C:\Cloudflared\cloudflared\bin\cloudflared.exe service install
 
 # Start Service
-$Service = Get-Service -Name "cloudflared"
+$Service = Get-Service -Name "cloudflared" -erroraction "silentlycontinue"
 
 if ($Service.Status -ne "Running") {
     Stop-Service -Name "cloudflared"
@@ -16,4 +16,5 @@ else {
 }
 
 Write-Host "Adding custom ImagePath to for Cloudflared to point to C:\Cloudflared\config.yml"
-C:\Cloudflared\cloudflared\Resources\ImagePath.reg
+Invoke-Command {reg import C:\Cloudflared\cloudflared\Resources\ImagePath.reg *>&1 | Out-Null}
+
